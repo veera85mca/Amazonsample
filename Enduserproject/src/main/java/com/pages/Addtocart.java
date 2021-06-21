50961cdfb41aa6393d5e1b0a099b3a8a2b929f4e
veera85mca <veera85mca@gmail.com>
2021-06-21 11:57:10 +0530

    Add to cart page script

--- Enduserproject/src/main/java/com/pages/Addtocart.java
+++ Enduserproject/src/main/java/com/pages/Addtocart.java
@@ -1,5 +1,26 @@
 package com.pages;
 
-public class Addtocart {
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+
+import com.webdriverback.Webdriveractions;
+
+public class Addtocart extends Webdriveractions{
+
+	public By _Listproduct = By.xpath("//div[@data-component-type='s-search-result']");
+	public static Map<String, Float> Productprice = new HashMap<>();	
+ 	
+	public void Selectfirstitem() {
+		try {
+			List<WebElement> _allsearch = getdriver().findElements(_Listproduct);
+			listwebelementdispalyed(_allsearch);
+		} catch (Exception e) {
+			
+		}
+	}
+	
 }
